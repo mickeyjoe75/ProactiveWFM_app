f6983c4ae83e968e0770df6736d5490001197e59
Joseph Manuel <mickeyjoe75@gmail.com>
2018-04-03 12:47:23 +0100

    updated files- db and db structure files

---
+++ ProActiveWFM_app/db/proactivewfm.sql
@@ -0,0 +1,16 @@
+DROP TABLE locations;
+DROP TABLE organisations;
+DROP TABLE departments;
+DROP TABLE teams;
+DROP TABLE jobTitles;
+DROP TABLE employees;
+DROP TABLE shifts;
+
+CREATE TABLE locations
+
+
+
+
+
+
+;
