666b642e51bf6f6e4af98b86c8b3d77ca57190a5
Joseph Manuel <mickeyjoe75@gmail.com>
2018-04-03 16:04:08 +0100

    updated files- db SQL files completed

--- ProActiveWFM_app/db/proactivewfm.sql
+++ ProActiveWFM_app/db/proactivewfm.sql
@@ -1,16 +1,60 @@
-DROP TABLE locations;
+/* DROP TABLE locations;
 DROP TABLE organisations;
 DROP TABLE departments;
 DROP TABLE teams;
 DROP TABLE jobTitles;
 DROP TABLE employees;
-DROP TABLE shifts;
+DROP TABLE shifts; */
 
-CREATE TABLE locations
+CREATE TABLE locations (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE organisations (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE departments (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE teams (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE jobTitles (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
+CREATE TABLE employees (
+  id SERIAL8 PRIMARY KEY,
+  firstName VARCHAR(255) NOT NULL,
+  surName VARCHAR(255) NOT NULL,
+  email VARCHAR(80),
+  contractedHrs DECIMAL,
+  startDate DATE,
+  dateOfBirth DATE,
+  locationId INT4,
+  organisationId INT4,
+  departmentId INT4,
+  teamId INT4,
+  jobtitleId INT4,
+  shiftId INT4
+);
 
-;
+CREATE TABLE shifts (
+  id SERIAL8 PRIMARY KEY,
+  label VARCHAR(255) NOT NULL,
+  shiftDate DATE,
+  shiftStartTime TIME,
+  shiftEndTime TIME,
+  shiftDuration DECIMAL,
+  breakStartTime TIME,
+  breakEndTime TIME,
+  breakDuration DECIMAL
+);
